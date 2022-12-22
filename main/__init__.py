f020f49de314cb9ba30304afac01381a65b0cfdd
maltexade1 <40648156+maltexade1@users.noreply.github.com>
2022-12-22 11:15:55 +0100

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -17,7 +17,7 @@ API_HASH = "0868ddd7eea45fada738581d03fbfe4c"
 BOT_TOKEN = "5711820837:AAHDHYkQy8QOxBABBA1_m_Mx4LBAMmL8mvw"
 SESSION = "BQBqOLTAFJKA12k6Qx5sWenbV2Q4cIF44310BV7Lqv0d6nKqG-CWUKK7
 FORCESUB = "maltexade_tv"
-AUTH = config("AUTH", default=None, cast=int)
+AUTH = 690195791
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
